f7bce9b9a8b5e44101c0fce58c4e4b121eea19a8
pabdelhay <pabloabdelhay@gmail.com>
2020-06-29 00:23:36 -0300

    Groups and is_staff on user admin

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -25,7 +25,7 @@ class CustomUserAdmin(CountryPermissionMixin, UserAdmin):
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
         (_('Permissions'), {
-            'fields': ('is_active', ),
+            'fields': ('is_active', 'is_staff', 'groups'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
