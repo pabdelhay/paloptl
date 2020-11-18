6fd2ccd17b6b685e84d7f796743e21b71c461593
pabdelhay <pabloabdelhay@gmail.com>
2020-11-18 13:11:20 -0300

    Sample chart

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from frontend.views import IndexView, CountryView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('dataviz/', TemplateView.as_view(template_name="frontend/chart_sample.htm"), name='dataviz'),
     path('<slug>/', CountryView.as_view(), name='country-details'),
 ]
