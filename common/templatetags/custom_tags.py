28ce71eccdf19f3ef80e38f97fb9fc9df38e7cc1
pabdelhay <pabloabdelhay@gmail.com>
2020-12-01 19:37:03 -0300

    Layout implementation on going

--- common/templatetags/custom_tags.py
+++ common/templatetags/custom_tags.py
@@ -2,6 +2,7 @@ from django import template
 from django.utils import timezone
 
 from apps.budget.models import Budget
+from apps.geo.models import Country
 
 register = template.Library()
 
@@ -22,3 +23,8 @@ def get_current_budget(user):
     if user.profile.country is not None:
         base_qs = base_qs.filter(country=user.profile.country)
     return base_qs.first()
+
+
+@register.simple_tag
+def get_countries():
+    return Country.objects.all().order_by('name')
