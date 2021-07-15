9864522367cd89892702678b9e8a63e4c79df46b
pabdelhay <pabloabdelhay@gmail.com>
2021-07-15 13:06:03 -0300

    Home css changes

--- apps/budget/models/__init__.py
+++ apps/budget/models/__init__.py
@@ -1,6 +1,6 @@
 from .budget import Budget
 from .upload import Upload
 from .agency import Agency
-from .function import Function
+from .expense_function import ExpenseFunction
 from .transparency_index import TransparencyIndex
 from .upload_log import UploadLog
