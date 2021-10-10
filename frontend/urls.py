7e8daabb7dfb947516f2af881db75bde08641824
pabdelhay <pabloabdelhay@gmail.com>
2021-10-09 23:10:25 -0300

    Cleaning unused files

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,6 @@ from frontend.views import IndexView, CountryView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('dataviz/', TemplateView.as_view(template_name="frontend/chart_sample.htm"), name='dataviz'),
     path('<slug>/', CountryView.as_view(), name='country-details'),
+    path('dataviz/', TemplateView.as_view(template_name="frontend/chart_sample.htm"), name='dataviz'),
 ]
