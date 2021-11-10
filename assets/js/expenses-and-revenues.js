0cae0deec8353bb6e3cd1c14622d915e5e28d1ed
personal-xpert <personal-antunes@hotmail.com>
2021-11-10 15:29:03 +0000

    bug fixes and code improvements

--- assets/js/expenses-and-revenues.js
+++ assets/js/expenses-and-revenues.js
@@ -16,10 +16,11 @@ function expenses_and_revenues(_data) {
         var chart = am4core.create('chartdiv', am4charts.XYChart)
         // chart.colors.step = 2;
         chart.colors.list = [
-        am4core.color("#ea7a71"),
-          am4core.color("#fbd1c1"),
-          am4core.color("#0F69A3"),
-          am4core.color("#98B8DA"),
+        am4core.color("#0F69A3"),
+            am4core.color("#98B8DA"),
+            am4core.color("#ea7a71"),
+            am4core.color("#fbd1c1"),
+
         ];
         chart.legend = new am4charts.Legend()
         // chart.legend.position = 'top'
@@ -82,10 +83,10 @@ function expenses_and_revenues(_data) {
         chart.maskBullets = false;
         chart.responsive.enabled = true;
 
-        createSeries("revenue_budget", "Dotação Despesa", 50, -40, -25);
-        createSeries("revenue_execution", "Execução Despesa", 30, -40, 10);
-        createSeries("expense_budget", "Dotação Receita", 50, 40, -25);
-        createSeries("expense_execution", "Execução Receita", 30, 40, 10);
+        createSeries("revenue_budget", "Dotação Receita", 50, -20, -30);
+        createSeries("revenue_execution", "Execução Receita", 30, -20, 10);
+        createSeries("expense_budget", "Dotação Despesa", 50, 30, -30);
+        createSeries("expense_execution", "Execução Despesa", 30, 30, 10);
 
     }); // end am4core.ready()
 }
