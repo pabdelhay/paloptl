8390aa6a76c502072ed856f2290fb23472061875
EstimaSahuma <estimaetilenoglicol@gmail.com>
2022-03-03 09:24:56 -0300

    change to exchange

--- common/methods.py
+++ common/methods.py
@@ -10,7 +10,7 @@ def money_display(amount, currency):
 def raw_money_display(amount):
     return Money(amount, settings.RAW_CURRENCY)
 
-def apply_change_euro(amount, currency):
+def apply_exchange_euro(amount, currency):
     api_key = settings.FIXED_ACESS_KEY_CURRENCY_CHANGE
     url = f"http://data.fixer.io/api/latest?access_key={api_key}&symbols=USD,AOA,CVE,XOF,MZN,STD&format=1"
     r = requests.get(url)
@@ -21,7 +21,7 @@ def apply_change_euro(amount, currency):
     return amount / rate
 
 
-def apply_change(base_currency):
+def apply_exchange(base_currency):
     api_key = settings.FIXED_ACESS_KEY_CURRENCY_CHANGE
     url = f"https://freecurrencyapi.net/api/v2/latest?apikey=b9b4ceb0-9634-11ec-ac62-af9b6e9fcc16&base_currency={base_currency}"
     r = requests.get(url)
